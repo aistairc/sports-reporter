d817ec9b967c3fbbf4f5d9dafcff2728682cc32b
isohyt <hyate.iso@gmail.com>
2020-01-15 19:58:58 +0900

    :bug: minor bug fix for setup file

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Setup Modified RotoWire
 
 git clone https://github.com/harvardnlp/boxscore-data.git
-tar -xzvf boxscore-data/rotowire.tar.bz2
+tar -jxvf boxscore-data/rotowire.tar.bz2
 git clone https://github.com/aistairc/rotowire-modified
 
 cd rotowire-modified
@@ -11,19 +11,17 @@ python script/generate_rotowire_modified.py --src_dir ../rotowire
 cd ..
 mv rotowire-modified/rotowire-modified rotowire_v2
 
-rm -rf data2text-1 boxscore-data rotowire
+rm -rf boxscore-data rotowire
 
 # Setup IE model
-
 git clone https://github.com/ratishsp/data2text-1.git
 cd data2text-1
 wget https://plu-aist.s3-ap-northeast-1.amazonaws.com/sports-reporter/ie-rotowire-modified.tar.gz
-tar -xzvf ie-rotowire-modified.tar.gz
+tar -xzvf ie-rotowire-modified.tar.gz --strip=1
 patch -p1 < rotowire-modified-ie.patch
 
 rm ie-rotowire-modified.tar.gz
 cd ..
-
 
 # Download vocab file and model dump
 wget https://plu-aist.s3-ap-northeast-1.amazonaws.com/sports-reporter/dump.tar.gz
